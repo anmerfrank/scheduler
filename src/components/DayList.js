76ceab022b099de2fbf83e455966782f01cb93fb
Anne Franklin <anmerfrank@gmail.com>
2019-10-28 18:21:28 -0700

    Initial commit. Listing Days I and II completed.

---
+++ src/components/DayList.js
@@ -0,0 +1,12 @@
+export default function Button(props) {
+  const buttonClass = classnames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+ 
+  return (
+<ul></ul>
+  );
+}
+
+<ul></ul>
